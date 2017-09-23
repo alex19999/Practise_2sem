9d22ef72d20ec67acbf8bc372f116b887e47437a
alex19999 <armen.arsen2011@yandex.ru>
2017-09-23 19:23:21 +0300

    Try to fix some problems with split

--- split.c
+++ split.c
@@ -5,38 +5,37 @@
 
 #define MAX_SYM 1000
 #define MAX_WORDS 100
-#define MAX_DIVIDER 100
+#define MAX_SEP 100
 
 
-void Split(char* str_in,char* divider,char** tokens,int* quantity) {
+void Split(char* str_in, char* separator, char** tokens, int* quantity) {
         int counter = 0;
         int iter = 0;
         char* istr;
         int length_of_token = 0;
-        tokens = (char**)calloc(MAX_WORDS,sizeof(char*));
+        tokens = (char**)calloc(MAX_WORDS, sizeof(char*));
         str_in[strlen(str_in) - 1] = 0;
-        divider[strlen(divider) - 1] = 0;
-        istr =(char*)calloc(MAX_DIVIDER,sizeof(char));
+        separator[strlen(separator) - 1] = 0;
         char* new_str = str_in;
-        strcat(str_in,divider);
-        if(str_in != NULL || divider != NULL) {
+        strcat(str_in, separator);
+        if(str_in != NULL && separator != NULL) {
                 for(iter == 0; iter <= strlen(str_in); iter++) {
-                        istr = strstr(str_in,divider);
+                        istr = strstr(str_in, separator);
                         length_of_token = strlen(str_in) - strlen(istr);
                         if(istr == NULL) {
-                                tokens[counter] = (char*)calloc(strlen(new_str),sizeof(char));
-                                strcpy(tokens[counter],new_str);
-                                counter++;
+                                tokens[counter] = (char*)calloc(strlen(new_str), sizeof(char));
+                                strcpy(tokens[counter], new_str);
                                 break;
                         } else {
-                                tokens[counter] = (char*)calloc(length_of_token,sizeof(char));
-                                strncpy(tokens[counter],str_in,length_of_token);
-                                counter++;
-                                str_in = istr + strlen(divider);
+                                tokens[counter] = (char*)calloc(length_of_token, sizeof(char));
+                                strncpy(tokens[counter], str_in, length_of_token);
+                                str_in = istr + strlen(separator);
 
                         }
+		free(tokens[counter]);
+		counter++;
                 }
         *quantity = counter;
-        printf("%d\n",*quantity);
         }
+	free(tokens);
 }
--- task_one.c
+++ task_one.c
@@ -6,25 +6,17 @@
 #include"split.h"
 #define MAX_SYM 1000
 #define MAX_WORDS 100
-<<<<<<< HEAD
 #define MAX_SEP 100
 
 void Split(char* str_in, char* separator, char** tokens, int* quantity);
-=======
 
-// delimeter или separator ... divider - делитель, а у вас тут разделитель
-#define MAX_DIVIDER 100
 
-// fixit: после запятой нужен пробел здесь и далее
-void Split(char* str_in,char* divider,char** tokens,int* quantity);
->>>>>>> 17d41dba9a1280a5ebbca4f396bf2bb1057d2a8e
 
 int main() {
 	char* str_in;
 	int cnt = 0;	
 	int* quantity;
 	char** tokens;
-<<<<<<< HEAD
 	char* separator;
 	quantity = (int*)calloc(MAX_WORDS, sizeof(int));
         str_in = (char*)calloc(MAX_SYM, sizeof(char));
@@ -33,62 +25,39 @@ int main() {
         printf("please, give me a symbol to divide\n");
 	fgets(separator, MAX_WORDS, stdin);
 	Split(str_in, separator, tokens, quantity);
-=======
-	char* divider;
-	quantity = (int*)calloc(MAX_WORDS,sizeof(int));
-        str_in = (char*)calloc(MAX_SYM,sizeof(char));
-	printf("please,give me a string to divide it into tokens\n");
-	fgets(str_in,MAX_SYM,stdin);
-        printf("please,give me a symbol to divide\n");
-	fgets(divider,MAX_WORDS,stdin);
-	Split(str_in,divider,tokens,quantity);
-	// fixit: на каждый вызов calloc должен быть вызов free
->>>>>>> 17d41dba9a1280a5ebbca4f396bf2bb1057d2a8e
 return 0;
 }
-
 
 
 void Split(char* str_in, char* separator, char** tokens, int* quantity) {
         int counter = 0;
         int iter = 0;
-	char* istr;
-	int length_of_token = 0;
-	tokens = (char**)calloc(MAX_WORDS, sizeof(char*));
-	str_in[strlen(str_in) - 1] = 0; 
-	separator[strlen(separator) - 1] = 0;
-	istr =(char*)calloc(MAX_SEP, sizeof(char));
-	char* new_str = str_in;
-<<<<<<< HEAD
-	strcat(str_in, separator);
-	if(str_in != NULL && separator != NULL) {
-=======
-	strcat(str_in,divider);
-	// fixit: похоже, что вместо || надо &&
-	if(str_in != NULL || divider != NULL) {
->>>>>>> 17d41dba9a1280a5ebbca4f396bf2bb1057d2a8e
-		for(iter == 0; iter <= strlen(str_in); iter++) {
-			istr = strstr(str_in, separator);
-			length_of_token = strlen(str_in) - strlen(istr);
-			if(istr == NULL) {
-				tokens[counter] = (char*)calloc(strlen(new_str), sizeof(char));
-				strcpy(tokens[counter], new_str);
-				counter++;
-				break;
-			} else {
-				tokens[counter] = (char*)calloc(length_of_token, sizeof(char));
-				strncpy(tokens[counter],str_in, length_of_token);
-				counter++;
-				str_in = istr + strlen(separator);
-				
-			}
-		}
+        char* istr;
+        int length_of_token = 0;
+        tokens = (char**)calloc(MAX_WORDS, sizeof(char*));
+        str_in[strlen(str_in) - 1] = 0;
+        separator[strlen(separator) - 1] = 0;
+        char* new_str = str_in;
+        strcat(str_in, separator);
+        if(str_in != NULL && separator != NULL) {
+                for(iter == 0; iter <= strlen(str_in); iter++) {
+                        istr = strstr(str_in, separator);
+                        length_of_token = strlen(str_in) - strlen(istr);
+                        if(istr == NULL) {
+                                tokens[counter] = (char*)calloc(strlen(new_str), sizeof(char));
+                                strcpy(tokens[counter], new_str);
+                                break;
+                        } else {
+                                tokens[counter] = (char*)calloc(length_of_token, sizeof(char));
+                                strncpy(tokens[counter], str_in, length_of_token);
+                                str_in = istr + strlen(separator);
+
+                        }
+		free(tokens[counter]);
+		counter++;
+                }
         *quantity = counter;
-<<<<<<< HEAD
-=======
-	// fixit: ф-я должна только находить разбиение строки на слова ... вывод снаружи где-то
-        // в следующем домашнем упражнении надо будет переиспользовать эту ф-ю ... там выводить разбиение на экран не надо
-        printf("%d\n",*quantity);
->>>>>>> 17d41dba9a1280a5ebbca4f396bf2bb1057d2a8e
-	}
+        }
+	free(tokens);
 }
+
