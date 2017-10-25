af98930f1058a3c93d4584aab23e5b9f86e43a38
alex19999 <armen.arsen2011@yandex.ru>
2017-10-25 17:24:36 +0300

    Fix problems with task_one

--- task_one.c
+++ task_one.c
@@ -8,38 +8,24 @@
 #define MAX_WORDS 100
 #define MAX_SEP 100
 
-void Split(char* str_in, char* separator, char** tokens, int* quantity);
-
-/*
-удаляйте лишние пустые строки
-*/
-
-
 int main() {
 	char* str_in;
 	int cnt = 0;	
 	int* quantity;
 	char** tokens;
 	char* separator;
-	tokens = (char**)calloc(MAX_WORDS, sizeof(char*));
+	tokens = (char**)calloc(MAX_WORDS, sizeof(char*)); // Выделение памяти для всей конструкции //
 	separator = (char*)calloc(MAX_SEP, sizeof(char));
 	quantity = (int*)calloc(MAX_WORDS, sizeof(int));
-        str_in = (char*)calloc(MAX_SYM, sizeof(char));
+    str_in = (char*)calloc(MAX_SYM, sizeof(char));
 	printf("please, give me a string to divide it into tokens\n");
 	fgets(str_in, MAX_SYM, stdin);
 	str_in[strlen(str_in) - 1] = 0;
-        printf("please, give me a symbol to divide\n");
+    printf("please, give me a symbol to divide\n");
 	fgets(separator, MAX_WORDS, stdin);
 	separator[strlen(separator) - 1] = 0;
 	Split(str_in, separator, tokens, quantity);
-	/*
-	fixit: "пожалуйста, введите ...", а никакой результат работы не выводится
-	*/
-	free(tokens);
-	/*
-	fixit: число вызовов calloc должно совпадать с числом вывозов free,
-	иначе это утечка памяти
-	*/
+	free(tokens); // Удаление всей конструкции //
 	free(separator);
 	free(quantity);
 	free(str_in);
@@ -55,11 +41,12 @@ void Split(char* str_in, char* separator, char** tokens, int* quantity) {
         char* new_str = str_in;
         strcat(str_in, separator);
         if(str_in != NULL && separator != NULL) {
+            printf("Result:\n");
                 for(iter == 0; iter <= strlen(str_in); iter++) {
                         istr = strstr(str_in, separator);
                         length_of_token = strlen(str_in) - strlen(istr);
                         if(istr == NULL) {
-                                tokens[counter] = (char*)calloc(strlen(new_str), sizeof(char));
+                                tokens[counter] = (char*)calloc(strlen(new_str), sizeof(char)); // Выделение памяти под конкретное число токенов //
                                 strcpy(tokens[counter], new_str);
                                 break;
                         } else {
@@ -68,8 +55,11 @@ void Split(char* str_in, char* separator, char** tokens, int* quantity) {
                                 str_in = istr + strlen(separator);
 
                         }
-		counter++;
+                        printf("%s ",tokens[counter]);                
+		                counter++;
+                        free(tokens[counter]); // Удаление токенов //
                 }
+                printf("\n");
         *quantity = counter;
         }
 }
