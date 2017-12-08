44c797b4cd769a4de767011de6edd38beaa648e5
alex19999 <armen.arsen2011@yandex.ru>
2017-12-08 10:20:41 +0300

    Remove necessary string

--- Dealine-10.11.2017/wash_wipe.c
+++ Dealine-10.11.2017/wash_wipe.c
@@ -4,7 +4,7 @@ int num_of_washed_dishes = 0;
 
 void* func_wash(void* fd_1){
     FILE* f_one = fopen("dirty.txt", "r");
-    FILE* f_two = fopen("washing_time.txt", "r");
+    FILE* f_two = fopen("washing_time_2.txt", "r");
     int fd = *((int*)fd_1);
     int iter = 0;
     int k = 0;
@@ -31,12 +31,12 @@ void* func_wash(void* fd_1){
                 time_for_washing[strlen(time_for_washing) - 1] = 0;
                 Split(time_for_washing, separator, tokens_dish_washtime, quantity);
                 if(!strcmp(tokens_dish_washtime[0], tokens_dirty_dish[0])){
-                    while(num_of_washed_dishes >= TABLE_LIMIT) sleep(0.1);
                     for(iter = 0; iter < atoi(tokens_dirty_dish[1]); iter++){
-                    sleep(atoi(tokens_dish_washtime[1]));
-                    printf("%s is washed\n", tokens_dirty_dish[0]);
-                    write(fd, tokens_dirty_dish[0], MAX_SYM*sizeof(char));
-                    num_of_washed_dishes++;
+                        while(num_of_washed_dishes >= 2) sleep(0.1);
+                        sleep(atoi(tokens_dish_washtime[1]));
+                        printf("%s is washed\n", tokens_dirty_dish[0]);
+                        write(fd, tokens_dirty_dish[0], MAX_SYM*sizeof(char));
+                        num_of_washed_dishes++;
                     }
                     break;
                 }  
@@ -59,7 +59,7 @@ void* func_wipe(void* fd_2){
     int fd = *((int*)fd_2);
     int num_of_bytes = 0;
     int* quantity;
-    FILE* f = fopen("wipe_time.txt", "r");
+    FILE* f = fopen("dry2.txt", "r");
     char* dish_for_wiping;
     char* time_for_wiping;
     char* separator = ":";
