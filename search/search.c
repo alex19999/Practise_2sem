f00dfc68c5598296fbf3ed2e3b762aaad0cc8d5d
alex19999 <armen.arsen2011@yandex.ru>
2017-12-12 11:00:55 +0300

    fixed some problems

--- search/search.c
+++ search/search.c
@@ -2,54 +2,54 @@
 
 void decrease(char* string) { //decreases our string-path
     int len = 0;
-	if(string == NULL) return;
-	len = strlen(string);
+    if(string == NULL) return;
+    len = strlen(string);
     while(len != 0 && string [len] != '/') {
         string [len] = '\0';
         len--;
     }
     if(len == 0) return;
     string[len] = 0;
-	return;
+    return;
 }
 
 char* increase(char* string, char* extra_dir) { //increases our string-path
     char* new_dir;
     new_dir = (char*)calloc(MAX_SIZE, sizeof(char));
-	strcpy(new_dir, string);
-	strcat(new_dir, "/");
-	strcat(new_dir, extra_dir);
-	return new_dir;
+    strcpy(new_dir, string);
+    strcat(new_dir, "/");
+    strcat(new_dir, extra_dir);
+    return new_dir;
 }
 
 
 int search(char *string, int deep, const char *file) {
-	DIR* dir;
+    DIR* dir;
     char* new_dir;
-	struct dirent *enter;
+    struct dirent *enter;
     int result;
     new_dir = (char*)calloc(MAX_SIZE, sizeof(char));
     if(deep < 0) {
         return -1;
     }
-	if((dir = opendir(string)) == NULL) {
-		perror("Can't open directory");
-		exit(1);
-	}
-	while((enter = readdir(dir)) != NULL) {
-		if(!strcmp(enter->d_name, file)) {
+    if((dir = opendir(string)) == NULL) {
+	    perror("Can't open directory");
+	    exit(1);
+    }
+    while((enter = readdir(dir)) != NULL) {
+	    if(!strcmp(enter->d_name, file)) {
             return 1;
         }
     }
     rewinddir(dir);
     while((enter = readdir(dir)) != NULL) {
-		if(enter->d_type == 4 && strncmp(".", enter -> d_name, 1) && strncmp("..", enter -> d_name, 2)) {
-		    new_dir = increase(string, enter->d_name);
+        if(enter->d_type == 4 && strncmp(".", enter -> d_name, 1) && strncmp("..", enter -> d_name, 2)) {
+            new_dir = increase(string, enter->d_name);
             result = search(new_dir, deep - 1, file);
             if(result == 1) { 
                 printf("File %s was founded in %s directory\n", file, new_dir);
                 return 1;
-            }  
+            }   
             if(result == -1) {
                 decrease(string);
             }
@@ -64,17 +64,17 @@ int search(char *string, int deep, const char *file) {
 
 int main(int argc, char** argv) {
     if(argc - 1 == 3) { 
-	        int deep = atoi(argv[2]);
-	        char* directory;
-	        char* file;
-            int res = 0; 
-            directory = (char*)calloc(MAX_SIZE, sizeof(char));
-            file = (char*)calloc(MAX_SIZE, sizeof(char));
-            strcpy(directory, argv[1]);
-            strcpy(file, argv[3]);
-            search(directory, deep, file);
-            free(directory);
-            free(file);
+        int deep = atoi(argv[2]);
+        char* directory;
+        char* file;
+        int res = 0; 
+        directory = (char*)calloc(MAX_SIZE, sizeof(char));
+        file = (char*)calloc(MAX_SIZE, sizeof(char));
+        strcpy(directory, argv[1]);
+        strcpy(file, argv[3]);
+        search(directory, deep, file);
+        free(directory);
+        free(file);
     } else {
             printf("Incorect number of arguments\n");
     }
