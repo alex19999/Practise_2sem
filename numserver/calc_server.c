129a91d4da1bf103782809368312cc4b9cf057e3
alex19999 <armen.arsen2011@yandex.ru>
2017-12-15 00:47:28 +0300

    Calculate server is ready

--- numserver/calc_server.c
+++ numserver/calc_server.c
@@ -6,6 +6,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 #include "split.h"
 
 #define MAX_SYM 1000
@@ -15,75 +17,114 @@
 #define SERVER 0
 
 typedef struct Full_Desriptor {
-    int who;
     int fd_1;
     int fd_2;
-    char* result;
+    int semid;
 } descr;
 
+void my_semop(int semid, int change_value) {
+	struct sembuf mybuf;
+	mybuf.sem_op = change_value;
+	mybuf.sem_flg = 0;
+	mybuf.sem_num = 0;
+	if(semid < 0) {
+		printf("Incorrect semid is given\n");
+		exit(-1);
+	}
+	if(semop(semid, &mybuf, 1) < 0) {
+		printf("Can't wait for condition\n");
+		exit(-1);
+	}
+}
+
 void* func_client(void* fd_1) {
-        descr fd = *((descr*)fd_1);
-        char *input;
-        int num = 0;
-        input = (char*)calloc(MAX_SYM, sizeof(char));
-        while (1) {
-            fgets(input, MAX_SYM*sizeof(char), stdin);
-            while(strlen(input) < 1) sleep(0.1);
-            printf("Client writing\n");
-            fd.fd_1 = open("fifo_to.fifo", O_WRONLY);
-            write(fd.fd_1, input, MAX_SYM*sizeof(char));
-            close(fd.fd_1);
-            fd.fd_2 = open("fifo_in.fifo", O_RDONLY);
-            while((num = read(fd.fd_2, input, MAX_SYM)) < 0) sleep(0.1) ;
-            printf("result = %s\n", input);
-            break;
+    descr fd = *((descr*)fd_1);
+    char *input;
+    int num_of_bytes = 0;
+    input = (char*)calloc(MAX_SYM, sizeof(char));
+	printf("Please, wait for your queue, if it will be necessary\n");
+	my_semop(fd.semid, -1);
+	printf("Give me arguments to multiply\n");
+    while (1) {
+        fgets(input, MAX_SYM*sizeof(char), stdin);
+        fd.fd_1 = open("fifo_to.fifo", O_WRONLY);
+        if((num_of_bytes = write(fd.fd_1, input, MAX_SYM)) < 0) {
+            printf("Can't write\n");
+            exit(-1);
         }
+        close(fd.fd_1);
+        fd.fd_2 = open("fifo_in.fifo", O_RDONLY);
+        if((num_of_bytes = read(fd.fd_2, input, MAX_SYM)) < 0) {
+            printf("Can't read\n");
+            exit(-1);
+        }
+        close(fd.fd_2);
+        printf("result = %s\n", input);
+        break;
+    }
+	my_semop(fd.semid, 1);
+	free(input);
 }
 
 
-int calculate_serv(char* operation) {
-	char* delimeter = "*";
+int calculate_serv(char* str_for_mult) {
+    char* delimeter = "*";
     char** tokens;
     int* num_of_args;
     int iter = 0;
+    int i = 0;
+    int counter = 0;
     int result = 1;
     tokens = (char**)calloc(MAX_NUM_OF_ARGS, sizeof(char*));
     for(iter = 0; iter < MAX_NUM_OF_ARGS; iter++) {
         tokens[iter] = (char*)calloc(MAX_SYM, sizeof(char));
     }
-	num_of_args = (int*)calloc(1, sizeof(int));
-	Split(operation, delimeter, tokens, num_of_args);
+    num_of_args = (int*)calloc(1, sizeof(int));
+    Split(str_for_mult, delimeter, tokens, num_of_args);
     if(*num_of_args < 2) { 
-   		exit(0);
+   	    exit(0);
     } else {
-        for(iter = 0; iter < *num_of_args; iter++) {
-            result = result * atoi(tokens[iter]);
+        for(i = 0; i < *num_of_args; i++) {
+            result = result * atoi(tokens[i]);
         }
     }
+	free(num_of_args);
+	for(counter = 0; counter < iter; counter++) {
+		free(tokens[counter]);
+	}
+	free(tokens);
 	return result;
 }
 
 void* func_server(void* fd_2){
     descr fd = *((descr*)fd_2);
+    my_semop(fd.semid, MAX_CLIENTS);
     int num_of_bytes = 0; 
     int iter = 0;
-    int result = 0;
+    int result = 1;
     int counter = 0;
     char* res;
     char *output;
     output = (char*)calloc(MAX_SYM, sizeof(char));
     res = (char*)calloc(MAX_SYM, sizeof(char));
-    fd.result = (char*)calloc(MAX_SYM, sizeof(char));
     do {
 	    fd.fd_2 = open("fifo_to.fifo", O_RDONLY);
-        printf("Server reading\n");
-        num_of_bytes = read(fd.fd_2, output, MAX_SYM);
-        printf("server = %s\n", output);
+        if((num_of_bytes = read(fd.fd_2, output, MAX_SYM)) < 0) {
+            printf("Can't read\n");
+            exit(-1);
+        }
+        close(fd.fd_2);
         result = calculate_serv(output);
         sprintf(res, "%d", result);
         fd.fd_1 = open("fifo_in.fifo", O_WRONLY);
-        write(fd.fd_1, res, MAX_SYM*sizeof(char));
+        if((num_of_bytes = write(fd.fd_1, res, MAX_SYM)) < 0) {
+            printf("Can't write\n");
+            exit(-1);
+        }
+        close(fd.fd_1);
 	} while(1);
+    free(res);
+    free(output);
 }
 
 int main(int argc, char **argv) {
@@ -94,13 +135,18 @@ int main(int argc, char **argv) {
         int status_2;
         pthread_t th_1;
         pthread_t th_2;
-        char *input;
-        char *output;
-        char* result;
         char* filename_1 = "fifo_to.fifo";
         char* filename_2 = "fifo_in.fifo";
         int size = 1;
-        result = (char*)calloc(MAX_SYM, sizeof(char));
+	    key_t key;
+	    if((key = ftok("1.c", 0)) < 0) {
+		printf("Can't get key\n");
+		exit(-1);
+	    }
+	    if((fd.semid = semget(key, 1, 0666| IPC_CREAT)) < 0) {
+		    printf("Can't get semid\n");
+		    exit(-1);
+	    }
         if(access(filename_1, F_OK) == -1)
         if (mknod(filename_1, S_IFIFO | 0666, 0) < 0) {
             exit(-1);
@@ -109,15 +155,12 @@ int main(int argc, char **argv) {
         if (mknod(filename_2, S_IFIFO | 0666, 0) < 0) {
             exit(-1);
         }
-        printf("here\n");
         if(serv_or_client == CLIENT) {
-                fd.who = CLIENT;
-                printf("Give me numbers to calculate\n");
                 status_2 = pthread_create(&th_2, NULL, func_client, &fd);
                 pthread_join(th_2, NULL);
 
         } else {
-            fd.who = SERVER;
+            printf("semid = %d\n", fd.semid);
             status_1 = pthread_create(&th_1, NULL, func_server, &fd);
             pthread_join(th_1, NULL);
         }
@@ -126,3 +169,5 @@ int main(int argc, char **argv) {
     }
         return 0;
 }
+
+
