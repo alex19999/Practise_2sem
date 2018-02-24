a74a6edc85fd57473625726231ea195d9242d090
alex19999 <armen.arsen2011@yandex.ru>
2018-02-24 12:35:21 +0300

    Add statistics

---
+++ counter/main.cpp
@@ -0,0 +1,28 @@
+#include"words_counter.h"
+
+int main() {
+    std::ifstream file("file.txt");
+    std::ofstream fout("fout.txt");
+    std::map<string, int> map;
+    if(file.is_open()) {
+        string word;
+        while(!file.eof()) {
+            file >> word;
+            word = prepare(word);
+            map[word]++;
+        }
+        std::vector< std::pair< string, int > > vec(map.begin(), map.end() );
+        std::sort(vec.begin(), vec.end(), compare);
+        for (auto p : vec) {
+            std::cout << p.first << ' ' << p.second << std::endl;
+            fout << p.first << ' ' << p.second << "\n";
+        }
+        fout.close();
+        file.close();
+    }
+}
+
+
+
+
+
