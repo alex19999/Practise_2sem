2a762b9cd0a84f5806396b1c8beb5738fee61937
Vlad <vlad.miryaha@gmail.com>
2018-02-24 19:30:34 +0300

    read comments

--- counter/main.cpp
+++ counter/main.cpp
@@ -1,4 +1,4 @@
-#include"words_counter.h"
+#include "words_counter.h"
 
 int main() {
     std::ifstream file("file.txt");
@@ -11,8 +11,19 @@ int main() {
             word = prepare(word);
             map[word]++;
         }
-        std::vector< std::pair< string, int > > vec(map.begin(), map.end() );
+        /*
+        вопрос дискуссионный, но когда пишут код помимо скорости, потребления памяти и т.п. уделяют большое внимание понятности (читаемости) кода.
+        из std::pair и последующего использования p.first, p.second неясно, что это за пара, какие данные хранит.
+        вероятно в данном случае отдельная структура с понятными названиями полей была бы предпочтительнее 
+        */
+        std::vector< std::pair< string, int > > vec(map.begin(), map.end());
         std::sort(vec.begin(), vec.end(), compare);
+        /*
+        ваша std::pair уже не совмем легкая, т.к. хранит строку.
+        написав auto p вы на каждой итерации цикла вызываете конструктор копирования для пары.
+        лучше написать const auto& p : vec,
+        копировать ссылку дешево, а const, чтобы случайно не изменить vec
+        */
         for (auto p : vec) {
             std::cout << p.first << ' ' << p.second << std::endl;
             fout << p.first << ' ' << p.second << "\n";
@@ -21,8 +32,4 @@ int main() {
         file.close();
     }
 }
-
-
-
-
 
