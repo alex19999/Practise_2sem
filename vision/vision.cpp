4f6cc84f5bd2f8dd5240ef4a73a62b65aab9b22c
alex19999 <armen.arsen2011@yandex.ru>
2018-04-03 00:07:05 +0300

    Just a template

--- vision/vision.cpp
+++ vision/vision.cpp
@@ -1,47 +1,62 @@
+#include <dlib/opencv.h>
+#include <opencv2/highgui/highgui.hpp>
 #include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing/render_face_detections.h>
+#include <dlib/image_processing.h>
 #include <dlib/gui_widgets.h>
-#include <dlib/image_io.h>
-#include <iostream>
 
 using namespace dlib;
 using namespace std;
 
-
-int main(int argc, char** argv)
-{  
+int main()
+{
     try
     {
-        if (argc == 1)
+        cv::VideoCapture cap(0);
+        if (!cap.isOpened())
         {
-            cout << "Give some image files as arguments to this program." << endl;
-            return 0;
+            cerr << "Unable to connect to camera" << endl;
+            return 1;
         }
 
-        frontal_face_detector detector = get_frontal_face_detector();
         image_window win;
 
-        // Loop over all the images provided on the command line.
-        for (int i = 1; i < argc; ++i)
+        frontal_face_detector detector = get_frontal_face_detector();
+        shape_predictor pose_model;
+        deserialize("shape_predictor_68_face_landmarks.dat") >> pose_model;
+
+        while(!win.is_closed())
         {
-            cout << "processing image " << argv[i] << endl;
-            array2d<unsigned char> img;
-            load_image(img, argv[i]);
-            
-            pyramid_up(img);
-            std::vector<rectangle> dets = detector(img);
+            // Grab a frame
+            cv::Mat temp;
+            if (!cap.read(temp))
+            {
+                break;
+            }
+            cv_image<bgr_pixel> cimg(temp);
 
-            cout << "Number of faces detected: " << dets.size() << endl;
+            std::vector<rectangle> faces = detector(cimg);
+            std::vector<full_object_detection> shapes;
+            for (unsigned long i = 0; i < faces.size(); ++i)
+                shapes.push_back(pose_model(cimg, faces[i]));
+
             win.clear_overlay();
-            win.set_image(img);
-            win.add_overlay(dets, rgb_pixel(255,0,0));
-
-            cout << "Hit enter to process the next image..." << endl;
-            cin.get();
+            win.set_image(cimg);
+            win.add_overlay(render_face_detections(shapes));
         }
     }
-    catch (exception& e)
+    catch(serialization_error& e)
     {
-        cout << "\nexception thrown!" << endl;
+        cout << "You need dlib's default face landmarking model file to run this example." << endl;
+        cout << "You can get it from the following URL: " << endl;
+        cout << "   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << endl;
+        cout << endl << e.what() << endl;
+    }
+    catch(exception& e)
+    {
         cout << e.what() << endl;
     }
 }
+
+
+
